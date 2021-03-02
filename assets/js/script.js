2ca6c9da7f21670dc23f4f547790412ebde855a0
SMakela <73409799+smakela13@users.noreply.github.com>
2021-03-02 03:45:25 -0500

    Added dynamic creation of rows and columns to JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,27 @@
-var timeEl = $("#timeCol");
+/* var timeEl = $("#timeCol");
 var eventEl = $("#eventCol");
-var saveEl = $("#saveCol");
+var saveEl = $("#saveCol"); */
+var plannerEl = $("#planner");
+
+plannerEl.empty();
 
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
-function renderTimes() {
-    
-}
 
 for (let hour = 8; hour <= 18; hour++) {
     var index = hour - 8;
 
     var rowEl = $("<div>");
-    rowEl.addClass("row");
+    rowEl.addClass("row", "time-block");
     rowEl.attr("workTime", hour);
     
-    var colEl = $("<div>");
-    colEl.addClass("col");
+    var timeEl = $("<div>");
+    timeEl.attr("id", "timeCol");
+    timeEl.addClass("col", "hour");
 
     var workHourEl = $("<span>");
-    workHourEl.addClass("workTime");
+    workHourEl.attr("id", "workTime");
 
     var isHour = 0;
     var amPM = "";
@@ -31,8 +32,35 @@ for (let hour = 8; hour <= 18; hour++) {
         isHour = hour;
         amPM = "AM";
     }
+    
+    timeEl.text("It's time");
+    rowEl.text("I'm rowing");
+    workHourEl.text("...to work");
+    plannerEl.append(rowEl);
+    rowEl.append(timeEl);
+    timeEl.append(workHourEl);
+
+    var eventEl = $("<div>");
+    eventEl.attr("id", "eventCol");
+
+    eventEl.addClass("col-6");
+    eventEl.addClass("past");
+    eventEl.addClass("present");
+    eventEl.addClass("future");
+
+    eventEl.text("be productive");
+    rowEl.append(eventEl);
+
+    var saveEl = $("<div>");
+    saveEl.attr("id", "saveCol");
+    saveEl.addClass("saveBtn i:hover col");
+    saveEl.text("be saved");
+    rowEl.append(saveEl);
 }
 
-rowEl.append(colEl);
+function rowColors() {
+    
+}
+
 
 moment().local();
