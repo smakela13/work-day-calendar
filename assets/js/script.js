a4cf625630fa5683a7eed540f8b38f7d4998561e
SMakela <73409799+smakela13@users.noreply.github.com>
2021-03-06 00:37:50 -0500

    Added ability to save info with save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,19 @@ var workStart = 7;
 var workEnd = 18;
 
 // planner container for the scheduler
-var plannerEl = $("#planner");
+var plannerEl = $('#planner');
 plannerEl.empty();
 
 // updates clock to allow the user to see current time
 $(document).ready(function () {
-    $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+    $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
 
     var update = function () {
         time = moment();
-        timing.html(time.format("h:mm:ss A"));
+        timing.html(time.format('h:mm:ss A'));
     };
 
-    timing = $("#currentTime");
+    timing = $('#currentTime');
     update();
     setInterval(update, 1000);
 });
@@ -24,28 +24,29 @@ $(document).ready(function () {
 for (let hour = workStart; hour <= workEnd; hour++) {
     var index = hour - 8;
     
-        var rowEl = $("<div>");
-        rowEl.addClass("row time-block");
-        rowEl.attr("workTime", hour);
+        // separate hour, description, and button into three functions
+        var rowEl = $('<div>');
+        rowEl.addClass('row time-block');
+        rowEl.attr('workTime', hour);
 
-        var timeEl = $("<div>");
-        timeEl.attr("id", "timeCol");
-        timeEl.addClass("col-1 hour");
+        var timeEl = $('<div>');
+        timeEl.attr('id', hour);
+        timeEl.addClass('col-1 hour');
 
-        var workHourEl = $("<span>");
-        workHourEl.attr("id", "workTime");
+        var workHourEl = $('<span>');
+        workHourEl.attr('id', 'workTime');
 
         var isHour = 0;
-        var amPM = "";
+        var amPM = '';
         if (hour > 12) {
             isHour = hour - 12;
-            amPM = "PM";
+            amPM = 'PM';
         } else if (hour < 12) {
             isHour = hour;
-            amPM = "AM";
+            amPM = 'AM';
         } else {
             isHour = 12;
-            amPM = "PM";
+            amPM = 'PM';
         }
 
         timeEl.text(isHour);
@@ -54,34 +55,51 @@ for (let hour = workStart; hour <= workEnd; hour++) {
         rowEl.append(timeEl);
         timeEl.append(workHourEl);
 
-        var eventEl = $("<div>");
-        eventEl.attr("id", "eventCol");
-        eventEl.addClass("col-10");
-        eventEl.text("be productive");
+        var eventEl = $('<div>');
+        eventEl.attr('id', 'eventCol');
+        eventEl.addClass('col-10');
+        eventEl.text('be productive');
+        // if has entry for hour(hour) {eventEl.text(getHourData(hour))}
+        // getHourData will access the localstorage and display the data
         rowEl.append(eventEl);
 
-        var iEl = $("<i>");
+        var iEl = $('<i>');
         rowEl.append(iEl);
     
-        var saveEl = $("<button>");
-        saveEl.attr("id", "saveCol");
-        saveEl.addClass("saveBtn col-1");
-        saveEl.text("be saved");
+        var saveEl = $('<button>');
+        saveEl.attr('id', 'saveCol');
+        saveEl.addClass('saveBtn col-1');
+        saveEl.text('be saved');
         rowEl.append(saveEl);
-
+        
+        
+        // add ability to get info from localstorage
+    
     rowColors(eventEl, hour);
+    getHourData();
 }
 
     // updates the row's colors for each of viewing day
     function rowColors(eventEl, hour) {
-        var currentTime = parseInt(moment().format("HH"));
+        var currentTime = parseInt(moment().format('HH'));
 
         if (hour < currentTime) {
-            eventEl.addClass("past");
+            eventEl.addClass('past');
         } else if (hour === currentTime) {
-            eventEl.addClass("present");
+            eventEl.addClass('present');
         } else {
-            eventEl.addClass("future");
+            eventEl.addClass('future');
         }
 
+    }
+    
+$('#saveCol').on('click', function () {
+    var hour = $('#timeCol').text();
+    var eventDesc = $('#eventCol').text();
+    
+    localStorage.setItem(hour, eventDesc);
+});
+    
+    function getHourData(hour) {
+        localStorage.getItem(hour);
     }
